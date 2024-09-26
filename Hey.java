23c992bdfda04d38c49169869c8b35daab0de2e0
vaditya kalyani <kalyanivaditya@gmail.com>
2024-09-26 16:56:56 +0530

    Added Hi.java Hello.java Hey.java Files  assigned to me

---
+++ Hey.java
@@ -0,0 +1,9 @@
+
+class Hey{
+
+public static void main(String args[]){
+
+ System.out.println("Say Hey");
+
+}
+}
---
+++ Hi.java
@@ -0,0 +1,20 @@
+class Hi{
+
+public static void main(String args[]){
+
+ System.out.println("say Hi");
+
+}
+}
+
+
+
+
+class Hey{
+
+public static void main(String args[]){
+
+ System.out.println("Say Hey");
+
+}
+}
